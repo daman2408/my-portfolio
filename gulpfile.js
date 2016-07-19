f0ae815e5f919b7e4c89ca03b05604578b026f6c
daman2408 <damanvir32@gmail.com>
2016-07-19 16:42:29 -0400

    changed colors

--- gulpfile.js
+++ gulpfile.js
@@ -2,6 +2,7 @@ var gulp = require('gulp');
 var cleanCSS = require('gulp-clean-css');
 var htmlmin = require('gulp-htmlmin');
 var imagemin = require('gulp-imagemin');
+var connect = require('gulp-connect');
 
 gulp.task('imagemin', function() {
   gulp.src('images/*')
@@ -13,11 +14,25 @@ gulp.task('imagemin', function() {
 gulp.task('minify-css', function() {
   return gulp.src('styles/*.css')
     .pipe(cleanCSS())
-      .pipe(gulp.dest('dist'));
+      .pipe(gulp.dest('dist'))
+        .pipe(connect.reload());
 });
 
 gulp.task('minify', function() {
   return gulp.src('./*.html')
     .pipe(htmlmin({collapseWhitespace: true}))
-    .pipe(gulp.dest('dist'))
+    .pipe(gulp.dest('dist'));
 });
+
+gulp.task('watch', function() {
+  gulp.watch('styles/portfolio.css', ['minify-css'])
+});
+
+gulp.task('connect', function() {
+  connect.server({
+    root:'/Volumes/My Passport for Mac/WebFiles/DamansPortfolio',
+    livereload: true
+  });
+});
+
+gulp.task('default', ['minify-css', 'connect', 'watch']);
